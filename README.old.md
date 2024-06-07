73f6492f8b95e01f01317c1abcdba45673339ed9
Keon <lune0410k@naver.com>
2024-06-07 23:47:05 +0900

    chore: file structure refactor

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# todo-project
-객체지향패러다임 수업 프로젝트 - TODO 어플리케이션 (React, Spring)
--- src/Hello.js
+++
@@ -1,9 +0,0 @@
-function Hello(props) {
-  return (
-    <div className="Hello">
-      <label for="hello"> 안녕하세요!</label>
-    </div>
-  );
-}
-
-export default Hello;
